36735aa63c3b613a33d096858787d99000c246dd
Jainam Shah <jainams@google.com>
2023-10-19 13:10:49 -0700

    Add mechanism to notify Dock
    Added DockEvents which consists of events that can be sent to the dock
    to trigger a change.
    
    Added a broadcast listener which would be used to listen to incoming
    events. The receiver is added to the lib's Manifest so only explicit
    intents can trigger the broadcast.
    
    Added a helper for senders to be able to send correct DockEvents to the
    receiver.
    
    Bug: 301483111
    Bug: 301482374
    Test: atest CarDockLibTests
    Change-Id: I4a25e9c7301d81fc772f824ef92ea9613017c574

---
+++ docklib/tests/src/com/android/car/docklib/events/DockEventSenderHelperTest.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.car.docklib.events;
+
+import static android.view.Display.DEFAULT_DISPLAY;
+
+import static com.android.car.docklib.events.DockEventsReceiver.EXTRA_COMPONENT;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import android.app.ActivityManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.android.car.docklib.R;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+@RunWith(AndroidJUnit4.class)
+public class DockEventSenderHelperTest {
+    private static final String DOCK_RECEIVER_PKG = "DOCK_RECEIVER_PKG";
+    @Mock
+    public ActivityManager.RunningTaskInfo mRunningTaskInfo;
+    @Mock
+    public Context mContext;
+    @Mock
+    public Intent mIntent;
+    @Mock
+    public ComponentName mAppComponent;
+    @Captor
+    public ArgumentCaptor<Intent> mIntentCaptor;
+    private DockEventSenderHelper mDockEventSenderHelper;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        when(mContext.getString(eq(R.string.config_dockViewPackage))).thenReturn(DOCK_RECEIVER_PKG);
+        mDockEventSenderHelper = new DockEventSenderHelper(mContext);
+    }
+
+    @Test
+    public void sendEventBroadcast_nonDefaultDisplay_broadcastNotSent() {
+        when(mRunningTaskInfo.getDisplayId()).thenReturn(DEFAULT_DISPLAY + 1);
+
+        mDockEventSenderHelper.sendEventBroadcast(DockEvent.LAUNCH, mRunningTaskInfo);
+
+        verify(mContext, never()).sendBroadcast(any(Intent.class));
+    }
+
+    @Test
+    public void sendEventBroadcast_noBastActivity_noBaseIntentComponent_broadcastNotSent() {
+        when(mRunningTaskInfo.getDisplayId()).thenReturn(DEFAULT_DISPLAY);
+        mRunningTaskInfo.baseActivity = null;
+        when(mIntent.getComponent()).thenReturn(null);
+        mRunningTaskInfo.baseIntent = mIntent;
+
+        mDockEventSenderHelper.sendEventBroadcast(DockEvent.LAUNCH, mRunningTaskInfo);
+
+        verify(mContext, never()).sendBroadcast(any(Intent.class));
+    }
+
+    @Test
+    public void sendEventBroadcast_broadcastSent_receiverPackageSet() {
+        when(mRunningTaskInfo.getDisplayId()).thenReturn(DEFAULT_DISPLAY);
+        mRunningTaskInfo.baseActivity = mAppComponent;
+
+        mDockEventSenderHelper.sendEventBroadcast(DockEvent.LAUNCH, mRunningTaskInfo);
+
+        verify(mContext).sendBroadcast(mIntentCaptor.capture());
+        Intent intentSent = mIntentCaptor.getValue();
+        assertThat(intentSent.getComponent()).isNotNull();
+        assertThat(intentSent.getComponent().getPackageName())
+                .isEqualTo(DOCK_RECEIVER_PKG);
+    }
+
+
+    @Test
+    public void sendEventBroadcast_launchEvent_broadcastSent() {
+        when(mRunningTaskInfo.getDisplayId()).thenReturn(DEFAULT_DISPLAY);
+        mRunningTaskInfo.baseActivity = mAppComponent;
+
+        mDockEventSenderHelper.sendEventBroadcast(DockEvent.LAUNCH, mRunningTaskInfo);
+
+        verify(mContext).sendBroadcast(mIntentCaptor.capture());
+        Intent intentSent = mIntentCaptor.getValue();
+        assertThat(intentSent.getAction()).isEqualTo(DockEvent.LAUNCH.toString());
+        assertThat(intentSent.getExtras()).isNotNull();
+        assertThat(intentSent.getExtras().getParcelable(EXTRA_COMPONENT, ComponentName.class))
+                .isEqualTo(mAppComponent);
+    }
+
+    @Test
+    public void sendEventBroadcast_pinEvent_broadcastSent() {
+        when(mRunningTaskInfo.getDisplayId()).thenReturn(DEFAULT_DISPLAY);
+        mRunningTaskInfo.baseActivity = mAppComponent;
+
+        mDockEventSenderHelper.sendEventBroadcast(DockEvent.PIN, mRunningTaskInfo);
+
+        verify(mContext).sendBroadcast(mIntentCaptor.capture());
+        Intent intentSent = mIntentCaptor.getValue();
+        assertThat(intentSent.getAction()).isEqualTo(DockEvent.PIN.toString());
+        assertThat(intentSent.getExtras()).isNotNull();
+        assertThat(intentSent.getExtras().getParcelable(EXTRA_COMPONENT, ComponentName.class))
+                .isEqualTo(mAppComponent);
+    }
+
+
+    @Test
+    public void sendEventBroadcast_unpinEvent_broadcastSent() {
+        when(mRunningTaskInfo.getDisplayId()).thenReturn(DEFAULT_DISPLAY);
+        mRunningTaskInfo.baseActivity = mAppComponent;
+
+        mDockEventSenderHelper.sendEventBroadcast(DockEvent.UNPIN, mRunningTaskInfo);
+
+        verify(mContext).sendBroadcast(mIntentCaptor.capture());
+        Intent intentSent = mIntentCaptor.getValue();
+        assertThat(intentSent.getAction()).isEqualTo(DockEvent.UNPIN.toString());
+        assertThat(intentSent.getExtras()).isNotNull();
+        assertThat(intentSent.getExtras().getParcelable(EXTRA_COMPONENT, ComponentName.class))
+                .isEqualTo(mAppComponent);
+    }
+}
