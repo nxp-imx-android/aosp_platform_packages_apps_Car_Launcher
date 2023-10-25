d44fa4186e0669ee64a382994ab04a50973ae6b4
Dan Nguyen <danzz@google.com>
2023-10-25 03:07:23 -0700

    Add View Controller and default app provider
    Use the default app provider to set up the Dock
    
    Bug: b/301483072
    Test: manual, atest CarDockLibTests
    Change-Id: Ib751cae621fa753973b2a4e81e0de679b5f3b216

--- docklib/src/com/android/car/docklib/view/DockAdapter.kt
+++ docklib/src/com/android/car/docklib/view/DockAdapter.kt
@@ -1,18 +1,23 @@
 package com.android.car.docklib.view
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.android.car.docklib.R
 import com.android.car.docklib.data.DockAppItem
+import java.util.function.Consumer
 
-class DockAdapter(private val numItems: Int) : RecyclerView.Adapter<DockItemViewHolder>() {
+class DockAdapter(
+        private val numItems: Int,
+        private val intentDelegate: Consumer<Intent>
+) : RecyclerView.Adapter<DockItemViewHolder>() {
 
     private val items: Array<DockAppItem?> = arrayOfNulls(numItems)
 
     override fun onCreateViewHolder(parent: ViewGroup, p1: Int): DockItemViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.dock_app_item_view, parent)
-        return DockItemViewHolder(view)
+        return DockItemViewHolder(view, intentDelegate)
     }
 
     override fun getItemCount() = numItems
